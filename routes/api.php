0fef3695ce722f884e2709e997b5f0d580d9eb30
fikretcure <fikretcure@gmail.com>
2023-02-10 20:07:50 +0300

    Updated api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name("authentication.")->prefix("authentication")->controller(AuthenticationController::class)->group(function () {
+    Route::name("register")->post("register", "register");
+    Route::name("login")->post("login", "login");
+    Route::name("check-token")->post("check-token", "check-token");
 });
