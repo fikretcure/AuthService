31090c049b3de8d446e7cbf1744954f8e9319d77
fikretcure <fikretcure@gmail.com>
2023-02-10 21:59:02 +0300

    Updated Token

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  *
@@ -23,8 +24,19 @@ class Token extends Model
         "refresh_expired_at"
     ];
 
+    /**
+     * @var string[]
+     */
     protected $casts = [
         "bearrer_expired_at" => "datetime",
         "refresh_expired_at" => "datetime",
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
