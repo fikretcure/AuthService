ace22a604cbc064624d96be2e048f72e8ac72ebf
fikretcure <fikretcure@gmail.com>
2023-02-10 20:11:05 +0300

    Updated api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::name("authentication.")->prefix("authentication")->controller(AuthenticationController::class)->group(function () {
-    Route::name("register")->post("register", "register");
     Route::name("login")->post("login", "login");
     Route::name("check-token")->post("check-token", "check-token");
 });
+
+Route::apiResources([
+    'users' => UserController::class
+]);
